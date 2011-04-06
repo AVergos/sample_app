d86f539dcfded55156a56f5e6e76ce47e29102da
AVergos <vergosgeorge@hotmail.com>
2011-04-06 10:52:27 +0300

    Added search with AJAX at users index

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,7 @@
+jQuery.ajaxSetup({
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+})
+
 $(function() {
   $("#usersindex .pagination a").live("click", function(){
     $.getScript(this.href);
@@ -6,6 +10,11 @@ $(function() {
 
   $("#users_search input").keyup(function() {
     $.get($("#users_search").attr("action"), $("#users_search").serialize(), null, "script")
+    return false;
+  });
+  
+  $("#follow_form").submit(function() {
+    $.post($(this).attr("action"), $(this).serialize(), null, "script");
     return false;
   });
 });
