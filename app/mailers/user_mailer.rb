3cd1bba6360fcb8b324e286668e2306c1bb97720
AVergos <vergosgeorge@hotmail.com>
2011-04-08 21:20:14 +0300

    follow/unfollow notification mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,12 @@ class UserMailer < ActionMailer::Base
     @url = "http://stormy-rain-34.heroku.com/signin"
     mail(:to => user.email, :subject => "Registered")
   end
+  
+  def follow_confirmation(user)
+    mail(:to => user.email, :subject => "Someone is following you!")
+  end
+  
+  def unfollow_confirmation(user)
+    mail(:to => user.email, :subject => "Someone no longer follows you :( ")
+  end
 end
